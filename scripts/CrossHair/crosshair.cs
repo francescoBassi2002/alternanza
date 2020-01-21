8c26c71fc00e7113696bd0a4b9021c266e305d69
ItzShekhawat <48554422+ItzShekhawat@users.noreply.github.com>
2020-01-21 00:47:00 -0800

    Add files via upload

---
+++ scripts/CrossHair/crosshair.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tobii.Gaming;
+
+public class crosshair : MonoBehaviour
+{
+
+
+    public float  GradY; 
+    private float rotationZ = 0f;
+    private float sensitivityZ = 2f;
+    public GameObject Player;               // object to follow
+    private Vector3 Location;               // vector for position to follow the ball
+    public float mouseSensitivity = 0.09f;  // the sensitivity of the movement of the character (speed)                                 
+    public Transform PlayerBody;            // to control the player's body
+    private float centroX = Screen.width/2; // I calculate the center of the screen on the X axis
+    private float centroY = Screen.height/2;// I calculate the center of the screen on the Y axis
+    float xrotation = 0f;  
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void MoveCrossHair(){
+
+    // I instantiate an object that will control eyes tracker
+    GazePoint  gazePoint = TobiiAPI.GetGazePoint();
+
+    // check that eyes tracker is available        
+    if(gazePoint.IsRecent()){
+         
+            // reposition the center and coordinates to make the screen move with the eye tracker
+
+            float xMouse = (gazePoint.Screen.x-centroX)*mouseSensitivity*Time.deltaTime;
+            float yMouse = (gazePoint.Screen.y-centroY)*mouseSensitivity*Time.deltaTime;
+
+            //transform.position = new Vector3(xMouse,yMouse,0f);
+            
+           
+        }
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCrossHair();
+    }
+}
---
+++ scripts/Floor_music/floor_music.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floor_music : MonoBehaviour
+{   
+    
+    public int speed = 4;
+    private int errorBug =11;
+    public Transform parent;
+    public GameObject[] prefab;  
+    public int resu;
+    public int height;
+    public int width;
+    public AudioSource NameMusic = null;
+    public GameObject cubo;
+    public GameObject cubo2;
+    private AudioSource audioSource;
+    public Vector3 startPos = new Vector3(-3.08f,-1.18f,-7.31f);
+    private float maxY = 5;
+   
+
+
+
+    private void Awake() {
+        
+        createdCube();
+    
+    }
+
+
+
+
+    private void createdCube(){
+        int i  = 0;
+        prefab = new GameObject[height * width];
+        for (int x = 0; x < height; x++)
+        {
+            for (int y = 0; y < width; y++,i++)
+            {
+                
+                    if((i%errorBug)==0){
+                        prefab[i]= Instantiate(cubo2, new Vector3(x,Random.Range(1,5),y), Quaternion.identity);
+                        prefab[i].transform.parent = parent;
+                    }else{
+                        prefab[i]= Instantiate(cubo, new Vector3(x,Random.Range(1,5),y), Quaternion.identity);
+                        prefab[i].transform.parent = parent;
+                    }
+                
+                    prefab[i].transform.position = new Vector3(x,prefab[i].transform.position.y,y);
+            }
+        }
+    }
+
+    private void ChangePosition(){
+        int i = 0;
+        for(int x = 0; x < height ; x++){
+            for (int y = 0; y < width; y++,i++)
+            {
+                if(prefab[i].transform.position.y == maxY){
+                    Debug.Log("maggiore");
+                    prefab[i].transform.Translate(0f,(maxY*Time.deltaTime)*-1,0f);
+                }
+                if(prefab[i].transform.position.y > maxY){
+                    //Debug.Log("minore");
+                    prefab[i].transform.Translate(0f,speed*Time.deltaTime,0f);
+                }
+                
+            }
+        }
+    }
+
+
+    void Start()
+    {
+       
+        transform.position = startPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ChangePosition();
+        transform.Translate(0f,0f,20*Time.deltaTime);
+    }
+}
+
+
+
+
+
